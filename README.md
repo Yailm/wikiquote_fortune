13ed9f1dc968c9bec547a6d5b62737f6b5df5caa
Yailm <Fiotose@gmail.com>
2017-05-25 18:13:22 +0800

    program is working now

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # wikiquote_fortune
-Retrieve quotes from wikiquote.org and generate fortune cookie
 
+Retrieve quotes from wikiquote.org and generate fortune cookie
 inspired by the `python-wikiquote` and `wikiquote_fortune`
 
-## Still under progress
+## Usage
+
+$ python wikiquote_fortune.py "beauty and the beast"    # TV show name
+There're no results matching "beauty and the beast".
+But some suggesions was provided:
+
+[1:Beauty and the Beast (2017 film)]
+    Beauty and the Beast is a 2017 American musical romantic fantasy film about a young intellectual girl named Belle, who is taken prisoner by a fearsome Beast in his enchanted castle and she learns to look beyond his appearance while evading a narcissistic hunter who seeks to take Belle for himself.
+
+[2:Beauty and the Beast (1991 film)]
+    Beauty and the Beast is a 1991 film about a young, beautiful French girl who falls in love with a hideous beast.
+
+[3:Beauty and the Beast (1987 TV series)]
+    Beauty and the Beast (1987-1990) was An Urban Fantasy series staring Linda Hamilton and Ron Perlman. After a brutal attack in central park ADA Catherine Chandler is brought into an underground society that lives in caves and tunnels deep under New York.
+
+action (1..3, others for aborted): 
+"beauty and the beast (2017 film).dat" created
+There were 62 strings
+Longest string: 3268 bytes
+Shortest string: 7 bytes
+
+Copy the generated files into `/usr/share/fortune`
--- wikiquote_fortune.py
+++ wikiquote_fortune.py
@@ -1,34 +1,48 @@
 #!/usr/bin/env python3
 # -*- python -*-
 
+import os
 import sys
 import json
 import urllib
 import aiohttp
 import asyncio
+import lxml.html
 
-site = 'https://en.wikiquote.org/wiki/'
-show = sys.argv[1]
+_site = 'https://en.wikiquote.org/wiki/'
+_api = 'https://en.wikiquote.org/w/api.php?action=opensearch&format=json' \
+        '&formatversion=2&search={}&namespace=0&limit=10&suggest=true'
 
 
 def print_help():
-    print("Usage: {} SHOW" % sys.argv[0])
+    print("Usage: {} SHOW".format(sys.argv[0]))
     print("     -- program to generate fortune-mod cookie from wikiquote")
 
 
-def soups_turn(source):
-    print(source)
+def generate_fortune(source):
+    tree = lxml.html.fromstring(source)
+    show_name = tree.get_element_by_id('firstHeading').text_content()
+    file_name = '{}'.format(show_name.lower())
+    elements = tree.get_element_by_id(
+        'mw-content-text').xpath('ul/li|dl|div[@id="toc"]')
+    l, r = 0, None
+    for i in range(len(elements)):
+        if elements[i].tag == 'div':
+            l = i+1
+        elif not r and elements[-i-1].tag == 'dl':
+            r = -i
 
-async def suggest(session):
-    _api = 'https://en.wikiquote.org/w/api.php?action=opensearch&format=json' \
-            '&formatversion=2&search={}&namespace=0&limit=10&suggest=true'
+    text_list = list(map(lambda x: x.text_content().strip(), elements[l:r]))
+    with open(file_name, 'w') as fo:
+        fo.write("\n%\n".join(text_list)+"\n%")
+    os.execvp('strfile', ('strfile', file_name))
+
+
+async def search(session, show):
     url = _api.format(urllib.parse.quote(show))
     async with session.get(url) as resp:
         suggestions = json.loads(await resp.text())
-        print('\033[31mThere were no results matching "{}".\033[0m'.format(show))
         if len(suggestions[1]) > 1:
-            print('But some suggesions was provided:\n')
-
             for index in range(len(suggestions[1])):
                 print("\033[33;1m[%d:%s]\033[0m"
                       % (index+1, suggestions[1][index]))
@@ -36,28 +50,30 @@ async def suggest(session):
                 if suggestions[2][index]:
                     print()
 
-            option = input("action (1..{}, q for aborted) :".format(index+1))
-            if option == 'q':
-                return 0
-            elif option in [str(x) for x in range(1, index+2)]:
-                async with session.get(suggestions[3][int(option)-1]) as resp:
-                    soups_turn(await resp.text())
+            option = input("option (1..{}, or abort): ".format(index+1))
+            if option in [str(x) for x in range(1, index+2)]:
+                return suggestions[3][int(option)-1]
+        else:
+            print('\033[31mThere\'re no results matching "{}".\033[0m'
+                  .format(show))
 
-async def main(loop):
+async def main(loop, show):
     async with aiohttp.ClientSession(loop=loop) as session:
-        url = site + urllib.parse.quote(show.replace(' ', '_'))
-        async with session.get(url) as resp:
+        page_url = await search(session, show)
+        if not page_url:
+            return
+        async with session.get(page_url) as resp:
             if resp.status == 200:
-                soups_turn(await resp.text())
+                generate_fortune(await resp.text())
             else:
-                await suggest(session)
-
+                print("Error: url is not available, program is now exiting.")
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
         print_help()
         sys.exit(0)
+    show = sys.argv[1]
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
+    loop.run_until_complete(main(loop, show))
     loop.close()
